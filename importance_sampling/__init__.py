ade60725a7c6eaf07111bdba5b5f157eb53659c8
Angelos Katharopoulos <katharas@gmail.com>
2017-08-02 17:01:40 +0200

    Bump the version to 0.2

--- importance_sampling/__init__.py
+++ importance_sampling/__init__.py
@@ -11,6 +11,6 @@ __license__ = "MIT"
 __maintainer__ = "Angelos Katharopoulos"
 __email__ = "angelos.katharopoulos@idiap.ch"
 __url__ = "http://www.idiap.ch/~katharas/importance-sampling/"
-__version__ = "0.1.2"
+__version__ = "0.2"
 
 from training import ImportanceTraining, ApproximateImportanceTraining
