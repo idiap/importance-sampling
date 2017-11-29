6c9ec2b88c815c47a027043b30b43789f3fc5472
Angelos Katharopoulos <katharas@gmail.com>
2017-11-29 12:40:28 +0100

    Add file header for layers.py

--- importance_sampling/layers.py
+++ importance_sampling/layers.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2017 Idiap Research Institute, http://www.idiap.ch/
+# Written by Angelos Katharopoulos <angelos.katharopoulos@idiap.ch>
+#
+
 from keras import backend as K
 from keras.engine import Layer
 from keras import initializers
