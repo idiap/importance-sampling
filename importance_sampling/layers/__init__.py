353bacda97d57b931ddac37ceeabdc9b47cd5b48
Angelos Katharopoulos <katharas@gmail.com>
2018-03-01 19:30:34 +0100

    Run the copyright headers script

--- importance_sampling/layers/__init__.py
+++ importance_sampling/layers/__init__.py
@@ -1,3 +1,7 @@
+#
+# Copyright (c) 2017 Idiap Research Institute, http://www.idiap.ch/
+# Written by Angelos Katharopoulos <angelos.katharopoulos@idiap.ch>
+#
 
 from .metrics import MetricLayer, TripletLossLayer
 from .normalization import BatchRenormalization, LayerNormalization, \
